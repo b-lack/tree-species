87cc92d45fc353ed423fc90381883698e19f93fd
b-lack <gerrit.balindt@b-lack.io>
2021-07-17 16:50:17 +0200

    add color "getColorFromId" method

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,3 +1,4 @@
 export declare function decode(speciesId: string, languageCode: string): any;
 export declare function encode(name: string, languageCode: string): string;
 export declare function getList(languageCode: string): [string, string[]][];
+export declare function getColorFromId(str: any): string;
--- src/index.ts
+++ src/index.ts
@@ -44,3 +44,19 @@ export function getList(languageCode:string) {
 
     return null
 }
+function intToRGB(i){
+    var c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    return "00000".substring(0, 6 - c.length) + c;
+}
+
+export function getColorFromId(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return intToRGB(hash);
+} 
+
